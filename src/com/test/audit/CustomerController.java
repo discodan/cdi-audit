d982f0c5bd3e3d649eea06e238f2b19e18a1ebeb
Eric McIntyre <emcintyre@idt911.com>
2013-11-14 16:25:43 -0700

    More progress made with auditing.

--- src/com/test/audit/CustomerController.java
+++ src/com/test/audit/CustomerController.java
@@ -1,37 +1,64 @@
 package com.test.audit;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateful;
+import javax.ejb.TransactionManagement;
+import javax.ejb.TransactionManagementType;
 import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceUnit;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceContextType;
+import javax.transaction.UserTransaction;
 
 import com.test.audit.entity.Customer;
 
 @SessionScoped
+@Stateful
+@LocalBean
 @Named("customerController")
+@TransactionManagement(TransactionManagementType.BEAN)
 public class CustomerController implements Serializable {
 
 	private static final long serialVersionUID = -6762408715002674143L;
 
-	@PersistenceUnit(name = "customerDB")
+	@PersistenceContext(name = "customerDB", type = PersistenceContextType.EXTENDED)
 	private EntityManager em;
+	@Inject
+	private UserTransaction txn;
 	private List<Customer> customerList;
+	private List<Customer> historyList = new ArrayList<>();
 	private Customer customer;
-	private Mode editMode;
+	private Mode mode;
 
 	@PostConstruct
 	public void refresh() {
 
-		customerList = em.createQuery("from Customer", Customer.class).getResultList();
+		em.clear();
+		try {
+			txn.begin();
+			customerList = em.createQuery("from Customer", Customer.class)
+					.getResultList();
+			txn.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
-	public List<Customer> getList() {
+	public List<Customer> getCustomerList() {
 
 		return customerList;
+	}
+
+	public List<Customer> getHistoryList() {
+
+		return historyList;
 	}
 
 	public Customer getCustomer() {
@@ -39,38 +66,105 @@ public class CustomerController implements Serializable {
 		return customer;
 	}
 
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
 	public boolean isEditing() {
 
-		return editMode != null;
+		return mode == Mode.ADD || mode == Mode.EDIT;
+	}
+	
+	public boolean isHistory() {
+		
+		return mode == Mode.HISTORY;
 	}
 
 	public void startAdd() {
 
 		customer = new Customer();
-		editMode = Mode.ADD;
+		mode = Mode.ADD;
+		beginTxn();
 	}
 
 	public void startEdit() {
 
-		editMode = Mode.EDIT;
+		beginTxn();
+		mode = Mode.EDIT;
+	}
+
+	public void showHistory() {
+
+		mode = Mode.HISTORY;
+	}
+	
+	public void hideHistory() {
+		
+		historyList.clear();
+		mode = null;
 	}
 
 	public void finishEdit() {
 
-		editMode = null;
+		// Save
+		if (mode == Mode.ADD) {
+			em.persist(customer);
+		}
+		commitTxn();
+		cleanup();
 	}
 
 	public void cancelEdit() {
 
-		editMode = null;
+		rollbackTxn();
+		cleanup();
 	}
-	
+
 	public void delete() {
-		
+
+		// Delete
+		beginTxn();
+		em.remove(customer);
+		commitTxn();
+		cleanup();
+	}
+
+	private void cleanup() {
+
+		customer = null;
+		mode = null;
+		refresh();
+	}
+
+	private void beginTxn() {
+
+		try {
+			txn.begin();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void commitTxn() {
+
+		try {
+			txn.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void rollbackTxn() {
+
+		try {
+			txn.rollback();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	private static enum Mode {
 
-		ADD, EDIT
+		ADD, EDIT, HISTORY
 	}
 }
--- src/com/test/audit/entity/Customer.java
+++ src/com/test/audit/entity/Customer.java
@@ -1,12 +1,20 @@
 package com.test.audit.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.envers.Audited;
 
 @Entity
+@Table(name = "customer")
+@Audited
 public class Customer {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String firstName;
 	private String lastName;
